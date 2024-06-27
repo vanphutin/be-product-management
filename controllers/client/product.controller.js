1993f93e3da87801aec749363a36e131f0b59e00
vanphutin2004 <vanphutin.contact@gmail.com>
2024-06-27 10:10:54 +0700

    chi tiet san pham

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,19 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    status: "active",
+    deleted: false,
+  });
+  const newProduct = products.map((item, index) => {
+    item.priceNew = (
+      (item.price * (100 - item.discountPercentage)) /
+      100
+    ).toFixed(0);
+    return item;
+  });
+  console.log(products);
   res.render("client/pages/products/index.pug", {
     pageTitle: "San pham",
+    products: newProduct,
   });
 };
